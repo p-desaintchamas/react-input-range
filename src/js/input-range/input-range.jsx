f213eefbd1e22c616ec137b28607dc8665ac94a1
philippe-kili <philippe@kili-technology.com>
2020-10-21 15:55:34 +0200

    Inspect props

--- src/js/input-range/input-range.jsx
+++ src/js/input-range/input-range.jsx
@@ -487,6 +487,9 @@ export default class InputRange extends React.Component {
     if (this.props.onChangeComplete && !isDefined(this.startValue)) {
       this.startValue = this.props.value;
     }
+    if (this.props.onClick){
+      console.log(this.props)
+    }
   }
 
   /**
